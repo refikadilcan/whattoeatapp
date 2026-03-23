e1c7e911eac134d715912f74d69a9800ccae3049
Refik Adilcan Egilmez <refikegilmez@gmail.com>
2026-03-23 10:17:01 +0100

    Initial Commit

---
+++ whattoeatapp/whattoeatappApp.swift
@@ -0,0 +1,17 @@
+//
+//  whattoeatappApp.swift
+//  whattoeatapp
+//
+//  Created by Refik Adilcan Eğilmez on 2026-03-23.
+//
+
+import SwiftUI
+
+@main
+struct whattoeatappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
